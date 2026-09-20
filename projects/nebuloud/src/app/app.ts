117f5eb334c31021a6f5638ec5213adf6885367b
Valentin <27902270+vdistortion@users.noreply.github.com>
2026-09-20 15:27:20 +0500

    feat: add navigation loading indicator

--- projects/nebuloud/src/app/app.ts
+++ projects/nebuloud/src/app/app.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+  RouterOutlet,
+} from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Header } from './components/header/header';
 
 @Component({
@@ -7,4 +15,22 @@ import { Header } from './components/header/header';
   imports: [RouterOutlet, Header],
   templateUrl: './app.html',
 })
-export class App {}
+export class App {
+  private readonly router = inject(Router);
+  private readonly destroyRef = inject(DestroyRef);
+
+  readonly navigationLoading = signal(false);
+
+  constructor() {
+    this.router.events.pipe(takeUntilDestroyed(this.destroyRef)).subscribe((event) => {
+      if (event instanceof NavigationStart) this.navigationLoading.set(true);
+      if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError
+      ) {
+        this.navigationLoading.set(false);
+      }
+    });
+  }
+}
